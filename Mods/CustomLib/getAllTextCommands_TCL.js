10c3543dc212ac0abd09e6639e929a1d738b64f4
Acedia <110048632+slothyace@users.noreply.github.com>
2024-09-01 06:02:24 +0800

    Add files via upload

--- Mods/CustomLib/getAllTextCommands_TCL.js
+++ Mods/CustomLib/getAllTextCommands_TCL.js
@@ -1,66 +1,42 @@
 module.exports = {
   data: {
-    name: "Get All Text Commands",
+    name: "Get Commands Info",
+    tags: "Library Of Conflict",
   },
   category: "Bot",
   info: {
-    source: "https://github.com/slothyace/BCS/tree/main/Mods/TairitsuCustomLib",
-    creator: "candiedapple",
-    donate: "https://buymeacoffee.com/candiedapple",
+    source: "https://github.com/slothyace/BCS/tree/main/Mods/CustomLib",
+    creator: "Conflict",
+    donate: "https://ko-fi.com/slothyacedia",
   },
   UI: [
     {
       element: "storageInput",
-      storeAs: "commandList",
-      name: "Store Command Infos in List As",
+      storeAs: "store",
+      name: "Store Commands in List As",
     },
   ],
 
-  compatibility: ["Any"],
-  run(values, message, client, bridge) {
-
-    try {
-      // Load the JSON data
-      const jsonData = require('../data.json');
-
-      // Access the commands array
-      const commands = jsonData.commands;
-      const folders = jsonData.folders;
-
-      // Check if the commands property is an array
-      if (!Array.isArray(commands)) {
-        throw new Error('Commands property is not an array');
-      }
-
-      // Initialize an array for command objects
-      const commandList = [];
-
-      // Extract command names, descriptions, triggers, and folder names
-      commands.forEach(command => {
-        if (command.trigger !== 'event') {
-          // Get the folder name using the folder ID
-          const folderId = command.folder[0]; // Assuming each command has only one folder ID
-          const folderName = folders[folderId] ? folders[folderId].name : `${values.noFolderName}`;
-
-          // Construct the command object
-          const commandObject = {
-            commandname: command.name,
-            commanddescription: command.description,
-            commandtrigger: command.trigger,
-            commandfolder: folderName
-          };
-
-          // Push the command object into the array
-          commandList.push(commandObject);
-        }
-      });
-
-      bridge.store(values.commandList, commandList);
-
-
-    } catch (error) {
-      console.error('Error loading or processing JSON data:', error);
-    }
-
+  subtitle: (data) => {
+    return `List stored as ${data.store}`;
   },
+
+  compatibility: ["Any"],
+
+  async run(values, message, client, bridge) {
+
+    const jsonData = require('../data.json');
+    const commands = jsonData.commands;
+
+    const commandList = [];
+
+
+    commands.forEach(command => {
+      if (command.trigger === 'textCommand') {
+        commandList.push(command.name);
+      }
+    });
+
+    bridge.store(values.commandList, commandList);
+  }
 };
