61e3fbfdd454d63a70212ba2ed6bd19f4e1ed007
Acedia <110048632+slothyace@users.noreply.github.com>
2024-10-30 12:31:05 +0800

    rconify

---
+++ Mods/rconCommander_MOD.js
@@ -0,0 +1,78 @@
+module.exports = {
+  data: {
+    name: "Send RCON Command"
+  },
+  category: "RCON",
+  info: {
+    source: "https://github.com/slothyace/BCS/tree/main/Mods",
+    creator: "Acedia",
+    donate: "https://ko-fi.com/slothyacedia",
+  },
+  modules: ["rcon"],
+  UI: [
+    {
+      element: "input",
+      storeAs: "ipAddress",
+      name: "RCON Server IP Address",
+    },
+    {
+      element: "input",
+      storeAs: "ipPort",
+      name: "RCON Server Port",
+    },
+    {
+      element: "input",
+      storeAs: "rconPassword",
+      name: "RCON Server Password",
+    },
+    "-",
+    {
+      element: "largeInput",
+      storeAs: "rconCommand",
+      name: "RCON Command",
+    },
+    {
+      element: "store",
+      storeAs: "rconResponse",
+      name: "Store Command Response As",
+    },
+    {
+      element: "actions",
+      storeAs: "actions",
+      name: "On Response, Run"
+    }
+  ],
+
+  subtitle: (values) => {
+    return `Send command: ${values.rconCommand} to ${values.ipAddress}:${values.ipPort}`
+  },
+
+  compatibility: ["Any"],
+
+  async run(values, interaction, client, bridge){
+    const Rcon = require("rcon")
+    ipAddr = bridge.transf(values.ipAddress)
+    ipPort = bridge.transf(values.ipPort)
+    rconPw = bridge.transf(values.rconPassword)
+    rconCm = bridge.transf(values.rconCommand)
+
+    rconServer = new Rcon(ipAddr, ipPort, rconPw)
+
+    rconServer.on("auth", function(){
+      console.log(`Connection to ${ipAddr}:${ipPort} establish`)
+      console.log(`Sending command: ${rconCm}`)
+      rconServer.send(rconCm)
+    }).on("response", function(str){
+      console.log("Response received: "+ str)
+      rconServer.disconnect()
+      bridge.store(values.rconResponse, str)
+      bridge.runner(values.actions)
+    }).on("end", function(){
+      console.log(`Connection to ${ipAddr}:${ipPort} dropped.`)
+    }).on("error", function(str){
+      console.log(`Error: ${str}`)
+    })
+
+    rconServer.connect()
+  }
+}
---
+++ Mods/rconListener_MOD.js
@@ -0,0 +1,83 @@
+module.exports = {
+  data: {
+    name: "RCON Listener",
+  },
+  category: "RCON",
+  info: {
+    source: "https://github.com/slothyace/BCS/tree/main/Mods",
+    creator: "Acedia",
+    donate: "https://ko-fi.com/slothyacedia",
+  },
+  modules: ["rcon"],
+  UI: [
+    {
+      element: "input",
+      storeAs: "ipAddress",
+      name: "RCON Server IP Address",
+    },
+    {
+      element: "input",
+      storeAs: "ipPort",
+      name: "RCON Server Port",
+    },
+    {
+      element: "input",
+      storeAs: "rconPassword",
+      name: "RCON Server Password",
+    },
+    "-",
+    {
+      element: "toggle",
+      storeAs: "maintain",
+      name: "Maintain Connection",
+    },
+    {
+      element: "store",
+      storeAs: "serverMessage",
+      name: "Store RCON Server Message As",
+    },
+    {
+      element: "actions",
+      storeAs: "toRunAct",
+      name: "Run actions"
+    }
+  ],
+
+  subtitle: (values) => {
+    return `Listen to ${values.ipAddress}:${values.ipPort}, maintained: ${values.maintain}`
+  },
+
+  compatibility: ["Any"],
+
+  async run(values, interaction, client, bridge){
+    const Rcon = require("rcon")
+
+    ipAddr = bridge.transf(values.ipAddress)
+    ipPort = bridge.transf(values.ipPort)
+    rconPw = bridge.transf(values.rconPassword)
+
+    const rconServer = new Rcon(ipAddr, ipPort, rconPw)
+
+    rconServer.on("auth", function(){
+      console.log(`Connection made with ${ipAddr}:${ipPort}, authentication success.`)
+    }).on("error", function(err){
+      console.log(`Error: ${str}`)
+    }).on("end", function(){
+      if (bridge.transf(values.maintain) == true){
+        console.log(`Connection with ${ipAddr}:${ipPort} dropped, attempting reconnecting.`)
+        rconServer.connect()
+      }
+    }).on("response", function (str) {
+      console.log(str)
+      bridge.store(values.serverMessage, str);
+      bridge.runner(values.toRunAct);
+    }).on("server", function (str) {
+      console.log(str)
+      bridge.store(values.serverMessage, str);
+      bridge.runner(values.toRunAct);
+    })
+    
+    rconServer.connect()
+
+  }
+}
